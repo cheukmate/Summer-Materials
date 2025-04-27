d29717db56ae58671b846de8766cf13a72858697
cheukmate <goonamonster3@gmail.com>
2025-04-27 14:46:49 -0400

    did the wrong commit, THIS is the TalonFX intake stuff
    i think this is wrong but i digress

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,13 +4,74 @@
 
 package frc.robot.subsystems;
 
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.util.PhoenixUtil;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
+  // wheels motors, make the config
+  SparkMax intakeWheels1 = new SparkMax(Constants.intakeWheels1, MotorType.kBrushless);
+  SparkMax intakeWheels2 = new SparkMax(Constants.intakeWheels2, MotorType.kBrushless);
+  private SparkMaxConfig SPARKconfig = new SparkMaxConfig();
+  private SparkMaxConfig globalConfig = new SparkMaxConfig();
+
+  // pivot motor, make the config
+  TalonFX pivotMotor = new TalonFX(Constants.pivotMotor);
+  private TalonFXConfiguration config;
+
   public Intake() {
+    // init configs
+    pivotMotor.getConfigurator().apply(config);
+    pivotMotor.setNeutralMode(NeutralModeValue.Brake);
     
+    // music is fun
+    config.Audio.AllowMusicDurDisable = true;
+
+    // enable limit switches
+    config.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+    config.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+    // give pid values (closed loop control)
+    config.Slot0.kP = 0;
+    config.Slot0.kI = 0;
+    config.Slot0.kP = 0;
+    // set voltage limits
+    config.Voltage.withPeakForwardVoltage(.8)
+      .withPeakReverseVoltage(-.8);
+    
+    
+    PhoenixUtil.tryUntilOk(5, () -> pivotMotor.getConfigurator().apply(config));
+
+
+    globalConfig
+    .smartCurrentLimit(30)
+    .idleMode(IdleMode.kBrake);
+    
+    SPARKconfig
+    .apply(globalConfig)
+    .follow(intakeWheels1);
+    
+    intakeWheels1.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    intakeWheels2.configure(SPARKconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
+// i think this is how it works...
+  public void setPivotPosition(double pos){
+    var request = new PositionDutyCycle(pos);
+      pivotMotor.setControl(request);
+  }
+
+
 
   @Override
   public void periodic() {
